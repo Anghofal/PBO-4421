168593a4a9021f90f81b5460612cd01c6419a974
Anghofal <asutedjaww@gmail.com>
2022-03-17 16:40:15 +0700

    tugas 3

---
+++ Tugas 3/Tugas3/src/main/kotlin/BukuDemo.kt
@@ -0,0 +1,6 @@
+class BukuDemo {
+    companion object{
+        var buku1 = arrayOf("Pemrograman Berbasis Objek dengan Java","Indrajani","Elexmedia Komputindo",2007)
+        var buku2 = arrayOf("Dasar Pemrograman Java","Abdul Kadir","Andi Offset","2004")
+    }
+}
